77120e86d02e709565d7a8f546598f5d4f546d20
Arnar Þórðarson <arnart08@ru.is>
2017-10-19 14:38:58 +0000

    fixes

--- src/main/java/is/ru/hugb/calc/CalcWeb.java
+++ src/main/java/is/ru/hugb/calc/CalcWeb.java
@@ -4,6 +4,7 @@ import static spark.Spark.*;
 
 public class CalcWeb {
     public static void main(String[] args) {
+		port(getHerokuPort());
         get("/", (req, res) -> {
             return "No route specified. Try /add/1,2";
         });
@@ -13,6 +14,13 @@ public class CalcWeb {
         );
     }
 
+	static int getHerokuPort() {
+		ProcessBuilder psb = new ProcessBuilder();
+		if(psb.environment().get("PORT") != null) {
+			return Integer.parseInt(psb.environment().get("PORT"));
+		}
+		return 4567;
+	}
     private static int add(String input) {
         StringCalculator Calculator = new StringCalculator();
         return Calculator.add(input);
